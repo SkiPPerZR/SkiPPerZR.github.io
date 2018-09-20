777ef1d21e98a1e53bf876bd28bbb69f79e67ae1
SkiPPerZR <skipperzr@gmail,com>
2018-09-20 23:53:56 +0300

    Polind

---
+++ Polind/main.js
@@ -0,0 +1,22 @@
+// window.onload = function() {
+//    var button = document.querySelectorAll('.search-button'); // Получение всех кнопок
+//    var input = document.querySelectorAll('.search-place'); // Поле поиска мест
+   
+//    for (let i = 0; i < button.length; i++) {
+//        const element = button[i];
+//         element.addEventListener('click', function(e) {
+//            element.classList.toggle('active'); // Замена фона кнопок на желтый
+//         });
+//    }
+//    input[0].addEventListener('keydown', function(e) {
+//     const r = Math.floor(Math.random() * (256));
+//     const g = Math.floor(Math.random() * (256));
+//     const b = Math.floor(Math.random() * (256));
+//     const result = '#' + r.toString(16) + g.toString(16) + b.toString(16);
+//     e.target.style.background = result;
+//    });
+// }
+
+
+
+
